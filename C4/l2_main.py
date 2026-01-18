b347e0b12261b0bd4237de0580d7fb9d2ffeb897
shseah601 <shseah601@gmail.com>
2026-01-18 16:55:35 +0800

    Added C4 L2

---
+++ C4/l2_main.py
@@ -0,0 +1,32 @@
+def bubble_sort(nums):
+    swapping = True
+    end = len(nums)
+
+    while swapping:
+        swapping = False
+
+        for i in range(1, end):
+            if nums[i - 1] > nums[i]:
+                nums[i - 1], nums[i] = nums[i], nums[i - 1]
+                swapping = True
+
+        end -= 1
+
+    return nums
+
+
+def bubble_sort_for_loop(nums):
+    n = len(nums)
+
+    for i in range(n):
+        swapping = False
+
+        for j in range(0, n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                swapping = True
+
+        if not swapping:
+            break
+
+    return nums
