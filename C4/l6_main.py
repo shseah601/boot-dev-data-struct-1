ceee07a51a40d3c29cac21d7be8620a6c1453e16
shseah601 <shseah601@gmail.com>
2026-01-22 01:34:10 +0800

    Added C4 L6

---
+++ C4/l6_main.py
@@ -0,0 +1,33 @@
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+
+
+
+def merge(first, second):
+
+    result = []
+    i = j = 0
+
+    while i < len(first) and j < len(second):
+        if first[i] < second[j]:
+            result.append(first[i])
+            i += 1
+        else:
+            result.append(second[j])
+            j += 1
+
+    result += first[i:]
+    result += second[j:]
+
+    return result
+
+
+
