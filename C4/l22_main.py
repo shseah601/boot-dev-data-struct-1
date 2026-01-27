4a00d8b4c8b2fc8d9b9c3fe308a0bca91fae6cbc
shseah601 <shseah601@gmail.com>
2026-01-27 16:24:42 +0800

    Added C4 L22

---
+++ C4/l22_main.py
@@ -0,0 +1,10 @@
+def selection_sort(nums):
+    for i in range(len(nums)):
+        smallest_idx = i
+
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+
+    return nums
